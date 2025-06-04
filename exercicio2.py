afb84aa30e2b62016b2477643d3aa580c3e4f79d
franciscoandradi <166152118+franciscoandradi@users.noreply.github.com>
2025-06-04 07:48:06 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,8 @@
+def dif(numero1, numero2):
+    diferenca = abs(numero1 - numero2)
+    return diferenca
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+print("a diferença é: ", dif(numero1, numero2))
+
